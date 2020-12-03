ba349d23759e99fe695c81b2236bd1828e5aa42f
Devon Garber <75411562+devon-garber@users.noreply.github.com>
2020-12-03 14:27:44 -0800

    Add files via upload

---
+++ AocDay1.1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AocDay1._1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input= System.IO.File.ReadLines("input1.1.txt").Select(x => Int32.Parse(x)).ToArray();
+
+            for (int i = 0; i < input.Length; ++i)
+            {
+                for (int j = i; j < input.Length; ++j)
+                {
+                    if (input[i] + input[j] == 2020)
+                    {
+                        Console.WriteLine(input[i] * input[j]);
+                    }
+                }
+            }
+        }
+    }
+}
