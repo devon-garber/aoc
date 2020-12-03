df20e1016b3dc9a6f7e4c8d86c1d3136611a3978
Devon Garber <75411562+devon-garber@users.noreply.github.com>
2020-12-03 15:18:39 -0800

    Add files via upload

---
+++ AocDay3.1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Aoc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> input = System.IO.File.ReadLines("input3.1.txt");
+
+            int treeHits = 0;
+            int positionInLine = 0;
+            foreach (string row in input.Skip(1))
+            {
+                positionInLine += 3;
+                if (positionInLine >= row.Length)
+                {
+                    positionInLine = positionInLine % row.Length;
+                }
+
+                if (row[positionInLine] == '#')
+                {
+                    ++treeHits;
+                }
+            }
+
+            Console.WriteLine(treeHits);
+        }
+    }
+}
