4bd8aa25d1c484dc194fb40c54feaff36ee2abdc
Devon Garber <75411562+devon-garber@users.noreply.github.com>
2020-12-03 15:06:45 -0800

    Add files via upload

---
+++ AocDay2.2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Aoc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> input = System.IO.File.ReadLines("input2.1.txt");
+
+            int validPasswords = 0;
+            foreach (string passwordPolicy in input)
+            {
+                Match match = Regex.Match(passwordPolicy, "([0-9]+)-([0-9]+) ([a-z]): (.*)");
+
+                int firstIndex = Int32.Parse(match.Groups[1].Value) - 1;
+                int secondIndex = Int32.Parse(match.Groups[2].Value) - 1;
+                char letter = Char.Parse(match.Groups[3].Value);
+                string password = match.Groups[4].Value;
+
+                if ((password[firstIndex] == letter && password[secondIndex] != letter) ||
+                    (password[firstIndex] != letter && password[secondIndex] == letter))
+                {
+                    validPasswords++;
+                }
+            }
+
+            Console.WriteLine(validPasswords);
+        }
+    }
+}
