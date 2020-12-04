cc3a45b169ddec6246c2905b12fe744782dd5dd8
Devon Garber <75411562+devon-garber@users.noreply.github.com>
2020-12-03 16:02:15 -0800

    Add files via upload

---
+++ AocDay3.2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Aoc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> input = System.IO.File.ReadLines("input3.1.txt");
+
+            long rightOneDownOne = CountTreeHits(input, 1, 1);
+            long rightThreeDownOne = CountTreeHits(input, 3, 1);
+            long rightFiveDownOne = CountTreeHits(input, 5, 1);
+            long rightSevenDownOne = CountTreeHits(input, 7, 1);
+            long rightOneDownTwo = CountTreeHits(input, 1, 2);
+
+            Console.WriteLine(rightOneDownOne * rightThreeDownOne * rightFiveDownOne * rightSevenDownOne * rightOneDownTwo);
+        }
+
+        private static int CountTreeHits(IEnumerable<string> input, int right, int downRow)
+        {
+            int treeHits = 0;
+            int positionInLine = 0;
+            IEnumerable<string> trimmedInput = GetTrimmedInput(input.ToArray(), downRow); // Get rid of any useless lines
+
+            foreach (string row in trimmedInput.Skip(1)) // Ignore first row
+            {
+                positionInLine += right;
+                if (positionInLine >= row.Length)
+                {
+                    positionInLine = positionInLine % row.Length;
+                }
+
+                if (row[positionInLine] == '#')
+                {
+                    ++treeHits;
+                }
+            }
+
+            return treeHits;
+        }
+
+        private static IEnumerable<string> GetTrimmedInput(string[] input, int downRow)
+        {
+            int rowCount = 0;
+            List<string> trimmedInput = new List<string>();
+            while (rowCount < input.Count())
+            {
+                trimmedInput.Add(input[rowCount]);
+                rowCount += downRow;
+            }
+
+            return trimmedInput;
+        }
+    }
+}
