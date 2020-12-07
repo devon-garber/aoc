544220a4b248cf1cb84c6ff8489e78515793e6c9
Devon Garber <75411562+devon-garber@users.noreply.github.com>
2020-12-06 18:23:21 -0800

    Add files via upload

---
+++ AocDay5.1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Aoc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> input = System.IO.File.ReadLines("input5.1.txt");
+
+            int highestBoardingId = 0;
+            foreach (string line in input)
+            {
+                string rowString = line.Substring(0, 7);
+                string columnString = line.Substring(7, 3);
+
+                int row = FindBinaryRepresentation(rowString);
+                int column = FindBinaryRepresentation(columnString);
+                int boardingId = (row * 8) + column;
+
+                if (boardingId > highestBoardingId)
+                {
+                    highestBoardingId = boardingId;
+                }
+            }
+
+            Console.WriteLine(highestBoardingId);
+        }
+
+        private static int FindBinaryRepresentation(string binaryString)
+        {
+            int row = 0;
+
+            for (int i = 0; i < binaryString.Length; ++i)
+            {
+                if (binaryString[i] == 'B' || binaryString[i] == 'R')
+                {
+                    row += (int)Math.Pow(2, (binaryString.Length - 1 - i));
+                }
+            }
+
+            return row;
+        }
+    }
+}
