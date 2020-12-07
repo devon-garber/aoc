51c3a406f3ef9d2613202f14dc0840766ff7d32e
Devon Garber <75411562+devon-garber@users.noreply.github.com>
2020-12-06 18:48:04 -0800

    Add files via upload

---
+++ AocDay6.1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Aoc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> input = System.IO.File.ReadLines("input6.1.txt");
+            int totalUniqueAnswers = 0;
+
+            StringBuilder fullGroup = new StringBuilder();
+            foreach (string line in input)
+            {
+                if (line == string.Empty)
+                {
+                    // Solve
+                    string groupAnswers = fullGroup.ToString();
+                    totalUniqueAnswers += groupAnswers.Distinct().Count();
+                    fullGroup.Clear();
+                }
+                fullGroup.Append(line);
+            }
+
+            totalUniqueAnswers += fullGroup.ToString().Distinct().Count(); // Also the last group
+
+            Console.WriteLine(totalUniqueAnswers);
+        }
+    }
+}
